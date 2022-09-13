ee31e496a7d4ce4d1ffe725ed4adcdc4dec6ee4e
Carl Hinderer <carl.hinderer4@gmail.com>
2022-09-13 11:51:18 -0600

    Added creation of new products and stores to test client.

--- api-client/change_client.py
+++ api-client/change_client.py
@@ -12,6 +12,10 @@ BASE_URL = os.environ.get('BASE_URL', 'http://localhost:5000')
 
 fake = Faker()
 
+CUSTOMER_IDS = []
+PRODUCT_IDS = []
+STORE_IDS = []
+
 
 def send_changes():
     while True:
@@ -21,18 +25,32 @@ def send_changes():
 
 
 def send_change():
-    send_new_customer()
+    create_new_customer()
 
 
-def send_new_customer():
-    url = f'{BASE_URL}/api/v1/customers'
-    requst_body = json.dumps(build_new_customer())
+def create_new_entity(url_segment, entity_details, id_list):
+    url = f'{BASE_URL}/api/v1/{url_segment}'
+    request_body = json.dumps(entity_details)
     headers = {'Content-type': 'application/json'}
-    
+
     try:
-        requests.post(url, data=requst_body, headers=headers)
+        response = requests.post(url, data=requst_body, headers=headers)
+        new_record_id = response.json()['id']
+        id_list.append(new_record_id)
     except Exception as err:
         print(err)
+
+
+def create_new_customer():
+    create_new_entity('customers', build_new_customer(), CUSTOMER_IDS)
+
+
+def create_new_product():
+    create_new_entity('products', build_new_product(), PRODUCT_IDS)
+
+
+def create_new_store():
+    create_new_entity('stores', build_new_store(), STORE_IDS)
 
 
 def build_new_customer():
@@ -47,6 +65,33 @@ def build_new_customer():
         'phone': fake.msisdn()[:10]
     }
     return customer
+
+
+def build_new_product():
+    product = {
+        'sku': fake.ean8(),
+        'name': fake.catch_phrase(),
+        'description': fake.bs(),
+        'color': fake.color_name(),
+        'brand': fake.company(),
+        'weight': random.randint(1, 1000),
+        'price': fake.pricetag()[1:]
+    }
+    return product
+
+
+def build_new_store():
+    store = {
+        'store_id': fake.ean8(),
+        'name': fake.street_name(),
+        'manager_name': fake.name(),
+        'street_address': fake.street_address(),
+        'city': fake.city(),
+        'state': fake.state_abbr(),
+        'zip_code': fake.postcode(),
+        'phone': fake.msisdn()[:10]
+    }
+    return store
 
 
 if __name__ == '__main__':
