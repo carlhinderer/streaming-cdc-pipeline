d459703f99326222c49dab7efd80efdebf15a8c7
Carl Hinderer <carl.hinderer4@gmail.com>
2022-09-13 13:46:19 -0600

    Finished api client.

--- api-client/change_client.py
+++ api-client/change_client.py
@@ -12,20 +12,69 @@ BASE_URL = os.environ.get('BASE_URL', 'http://localhost:5000')
 
 fake = Faker()
 
+
+def fake_phone_number():
+    return fake.msisdn()[:10]
+
+
+def fake_weight():
+    return random.randint(1, 1000)
+
+
+def fake_price():
+    return random.randint(1, 100000)
+
+
 CUSTOMER_IDS = []
 PRODUCT_IDS = []
 STORE_IDS = []
 
 
-def send_changes():
-    while True:
-        send_change()
-        sleep_time = random.randint(1, 100) // 10   # Sleep from 0.1 - 10 s
-        time.sleep(sleep_time)
+CUSTOMER_FAKER_MAPPINGS = {
+    'email': fake.email,
+    'first_name': fake.first_name,
+    'last_name': fake.last_name,
+    'street_address': fake.street_address,
+    'city': fake.city,
+    'state': fake.state_abbr,
+    'zip_code': fake.postcode,
+    'phone': fake_phone_number
+}
+
+CUSTOMER_UPDATABLE_FIELDS = ['first_name', 'last_name', 'street_address', 'city',
+        'state', 'zip_code', 'phone']
 
 
-def send_change():
-    create_new_customer()
+PRODUCT_FAKER_MAPPINGS = {
+    'sku': fake.ean8,
+    'name': fake.catch_phrase,
+    'description': fake.bs,
+    'color': fake.color_name,
+    'brand': fake.company,
+    'weight': fake_weight,
+    'price': fake_price
+}
+
+PRODUCT_UPDATABLE_FIELDS = ['name', 'description', 'color', 'brand', 'weight',
+    'price']
+
+
+STORE_FAKER_MAPPINGS = {
+    'store_id': fake.ean8,
+    'name': fake.street_name,
+    'manager_name': fake.name,
+    'street_address': fake.street_address,
+    'city': fake.city,
+    'state': fake.state_abbr,
+    'zip_code': fake.postcode,
+    'phone': fake_phone_number
+}
+
+STORE_UPDATABLE_FIELDS = ['name', 'manager_name', 'street_address', 'city', 
+    'state', 'zip_code', 'phone']
+
+
+##################################################################################
 
 
 def create_new_entity(url_segment, entity_details, id_list):
@@ -34,7 +83,7 @@ def create_new_entity(url_segment, entity_details, id_list):
     headers = {'Content-type': 'application/json'}
 
     try:
-        response = requests.post(url, data=requst_body, headers=headers)
+        response = requests.post(url, data=request_body, headers=headers)
         new_record_id = response.json()['id']
         id_list.append(new_record_id)
     except Exception as err:
@@ -53,45 +102,114 @@ def create_new_store():
     create_new_entity('stores', build_new_store(), STORE_IDS)
 
 
+##################################################################################
+
+
+def delete_entity(url_segment, id_list):
+    if not id_list:
+        return
+
+    id_to_delete = random.choice(id_list)
+    url = f'{BASE_URL}/api/v1/{url_segment}/{id_to_delete}'
+
+    try:
+        requests.delete(url)
+        id_list.remove(id_to_delete)
+    except Exception as err:
+        print(err)
+
+
+def delete_customer():
+    delete_entity('customers', CUSTOMER_IDS)
+
+
+def delete_product():
+    delete_entity('products', PRODUCT_IDS)
+
+
+def delete_store():
+    delete_entity('stores', STORE_IDS)
+
+
+##################################################################################
+
+def update_entity(url_segment, updatable, faker_mappings, id_list):
+    if not id_list:
+        return
+
+    field_to_update = random.choice(updatable)
+    new_value_function = faker_mappings[field_to_update]
+    new_value = new_value_function()
+
+    id_to_update = random.choice(id_list)
+    url = f'{BASE_URL}/api/v1/{url_segment}/{id_to_update}'
+    request_body = json.dumps({field_to_update: new_value})
+    headers = {'Content-type': 'application/json'}
+
+    try:
+        requests.put(url, data=request_body, headers=headers)
+    except Exception as err:
+        print(err)    
+
+
+def update_customer():
+    update_entity('customers', CUSTOMER_UPDATABLE_FIELDS, 
+        CUSTOMER_FAKER_MAPPINGS, CUSTOMER_IDS)
+
+
+def update_product():
+    update_entity('products', PRODUCT_UPDATABLE_FIELDS, 
+        PRODUCT_FAKER_MAPPINGS, PRODUCT_IDS)
+
+
+def update_store():
+    update_entity('stores', STORE_UPDATABLE_FIELDS,
+        STORE_FAKER_MAPPINGS, STORE_IDS)
+
+
+
+##################################################################################
+
+def build_new_entity(mappings):
+    return {k: v() for (k, v) in mappings.items() }
+
+
 def build_new_customer():
-    customer = {
-        'email': fake.email(),
-        'first_name': fake.first_name(),
-        'last_name': fake.last_name(),
-        'street_address': fake.street_address(),
-        'city': fake.city(),
-        'state': fake.state_abbr(),
-        'zip_code': fake.postcode(),
-        'phone': fake.msisdn()[:10]
-    }
-    return customer
+    return build_new_entity(CUSTOMER_FAKER_MAPPINGS)
 
 
 def build_new_product():
-    product = {
-        'sku': fake.ean8(),
-        'name': fake.catch_phrase(),
-        'description': fake.bs(),
-        'color': fake.color_name(),
-        'brand': fake.company(),
-        'weight': random.randint(1, 1000),
-        'price': fake.pricetag()[1:]
-    }
-    return product
+    return build_new_entity(PRODUCT_FAKER_MAPPINGS)
 
 
 def build_new_store():
-    store = {
-        'store_id': fake.ean8(),
-        'name': fake.street_name(),
-        'manager_name': fake.name(),
-        'street_address': fake.street_address(),
-        'city': fake.city(),
-        'state': fake.state_abbr(),
-        'zip_code': fake.postcode(),
-        'phone': fake.msisdn()[:10]
-    }
-    return store
+    return build_new_entity(STORE_FAKER_MAPPINGS)
+
+
+CHANGE_METHODS = [
+    create_new_customer,
+    update_customer,
+    delete_customer,
+    create_new_product,
+    update_product,
+    delete_product,
+    create_new_store,
+    update_store,
+    delete_store
+]
+
+
+def send_changes():
+    while True:
+        send_change()
+        sleep_time = random.randint(1, 100) // 10   # Sleep from 0.1 - 10 s
+        time.sleep(sleep_time)
+
+
+def send_change():
+    change_fuction = random.choice(CHANGE_METHODS)
+    change_fuction()
+
 
 
 if __name__ == '__main__':
