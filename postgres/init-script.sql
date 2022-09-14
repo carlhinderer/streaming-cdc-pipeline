a0bb47e8d0f3762da4a13cfa51c139b849de2e20
Carl Hinderer <carl.hinderer4@gmail.com>
2022-09-14 14:06:10 -0600

    Updating Postgres configuration to work with Debezium.

--- postgres/init-script.sql
+++ postgres/init-script.sql
@@ -1,3 +1,6 @@
 CREATE DATABASE retaildb;
 CREATE USER retailuser WITH PASSWORD 'retailpw';
 GRANT ALL PRIVILEGES ON DATABASE retaildb TO retailuser;
+
+-- Replication settings for Debezium
+ALTER ROLE "retailuser" WITH LOGIN REPLICATION SUPERUSER;
